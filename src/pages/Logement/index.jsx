352a0aef13373056e8d8830e7524f3875f74cccb
alexvtl <alexandre.vital@hotmail.fr>
2023-02-08 13:47:07 +0100

    test axios logement

--- src/pages/Logement/index.jsx
+++ src/pages/Logement/index.jsx
@@ -1,12 +1,12 @@
 import { useParams } from 'react-router-dom';
-//import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Carrousel from '../../components/Carrousel/carrousel.jsx';
 import api from '../../Products/product.json';
 import Tags from '../../components/Tags/tags.jsx';
 import Rate from '../../components/Rate/rate.jsx';
 import Collapse from '../../components/Collapse/index.jsx';
-import Error from '../Error/index';
 import './logement.scss';
+import axios from 'axios';
 
 function Logement() {
   const { id } = useParams();
@@ -14,22 +14,23 @@ function Logement() {
   const foundLogement = api.find((logement) => {
     return logement.id === id;
   });
+  const [daatta, savedata] = useState([]);
 
-  //useEffect(() => {
-  //const Apilogement = () => {
-  //fetch('/Datas/data.json')
-  // .then((res) => res.json())
-  // .then((data) => {
-  //   saveData(data);
-  //const array = Data.filter((element) => element.id === id);
-  //  });
-  // };
-  // Apilogement();
-  //  }, []);
-  const erreur = foundLogement === undefined || foundLogement === null;
-  return erreur ? (
-    <Error />
-  ) : (
+  useEffect(() => {
+    //=> permet d'effectuer une action, ici une requête GET, à un moment donné du cycle de vie du composant
+    const displayLocation = async () => {
+      const result = await axios.get('/Datas/data.json'); //axios va me chercher l'Url, récupère les data sans besoin de convertir au format json
+      const logement = result.data.find((item) => item.id === id);
+
+      savedata(logement);
+    };
+    displayLocation();
+    // eslint-disable-next-line
+  }, []);
+
+  console.log(daatta);
+
+  return (
     <div className="logement">
       <Carrousel key={foundLogement.id} slides={foundLogement.pictures} />
       <div className="logement_info_box">
